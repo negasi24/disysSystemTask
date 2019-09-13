35ca04cb0468cd74a3f4afff7c17e3c3151fb6f2
Deepan\Deepan Negasi <deepan.negasi@gmail.com>
2019-09-13 11:42:36 +0530

    upgraded for firebase authentication and remote config

--- app/src/main/java/com/disys/systemtask/utility/SessionSharPref.java
+++ app/src/main/java/com/disys/systemtask/utility/SessionSharPref.java
@@ -2,18 +2,20 @@ package com.disys.systemtask.utility;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Base64;
 
 import com.disys.systemtask.model.User;
+
+import java.nio.charset.StandardCharsets;
 
 public class SessionSharPref {
     // User name (make variable public to access from outside)
 
-    public static final String key_eid="key_eid";
+
     public static final String key_name="key_name";
-    public static final String key_idbarahno="key_idbarahno";
-    public static final String key_emailaddress="key_emailaddress";
-    public static final String key_unifiednumber="key_unifiednumber";
-    public static final String key_mobileno="key_mobileno";
+    public static final String key_consumerSec="key_consumerSec";
+    public static final String key_consumerKey="key_consumerKey";
+
     public static final String key_password="key_password";
     public static final String key_new_user="key_new_user";
 
@@ -48,10 +50,44 @@ public class SessionSharPref {
 
     public void saveUserDate(User data)
     {
-        editor.putString(key_name, data.getUserName());
-        editor.putString(key_password, data.getPassword());
+        editor.putString(key_name,encryptData( data.getUserName()));
+        editor.putString(key_password,encryptData( data.getPassword()));
 
         editor.commit();
+    }
+
+    public void saveSecretCode(String s1,String s2)
+    {
+        editor.putString(key_consumerKey,encryptData(s1));
+        editor.putString(key_consumerSec,encryptData(s2));
+
+        editor.commit();
+    }
+
+    public String getConsumerKey()
+    {
+        return decriptData(pref.getString(key_consumerKey,""));
+    }
+    public String getConsumerSec()
+    {
+        return decriptData(pref.getString(key_consumerSec,""));
+    }
+
+
+    private String encryptData(String str)
+    {
+        byte[] data = str.getBytes(StandardCharsets.UTF_8);
+        String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+
+        return base64;
+    }
+
+    private String decriptData(String base64)
+    {
+        byte[] data = Base64.decode(base64, Base64.DEFAULT);
+        String text = new String(data, StandardCharsets.UTF_8);
+
+        return text;
     }
 
     public User getUserData() {
